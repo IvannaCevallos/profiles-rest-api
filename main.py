3ade5b2b83edafdf319b99c1e857b7b1edac087d
Ivanna Cevallos <ivanna.cevallos@thoughtworks.com>
2024-11-08 12:01:36 -0500

    Dice simulator

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-from random import randint, random
+import random
+from multiprocessing.managers import Value
 
-lower_num , higher_num = 1,10
-random_number: int = randint(lower_num, higher_num)
 
-print(f"Guess the number in the range from  {lower_num} to {higher_num}")
-
-while True:
-    try:
-        user_guess : int = int(input("Guess : "))
-    except ValueError as e:
-        print(f"please enter a valid number not this {e}")
-        continue
-    if user_guess > random_number:
-        print("the number is lower")
-    elif user_guess <  random_number:
-        print("the number is higher")
-    else:
-        print("You guessed it")
-        break
-
+def roll_dice(ammount : int = 2)->tuple [ list[int], int]:
+    if ammount <= 0 :
+        raise Value
+    rolls: list[int] = []
+    sum: int = 0
+    for i in range(ammount):
+        random_roll : int = random.randint(1,6)
+        rolls.append(random_roll)
+        sum += random_roll
+    return rolls, sum
+def main():
+    while True:
+        try:
+            user_input :str = input("How many dice would you like to roll ")
+            if user_input.lower() == "x":
+                print("Thanks for playing")
+                break
+            rolls, sum = roll_dice(int(user_input))
+            #print(*roll_dice(int(user_input)), sep=' - ')
+            print(f"Rolls {rolls}")
+            print(f"sum {sum}")
+        except ValueError as e:
+            print(f"I dont understand {e}")
+main()
