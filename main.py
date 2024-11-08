80b8d8fd4739680c7a875f332a3650ecc91c617c
Ivanna Cevallos <ivanna.cevallos@thoughtworks.com>
2024-11-08 11:38:02 -0500

    Number Guessing Game

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
-def get_input(word_type : str):
-    user_input : str = input(f"Enter a {word_type}:")
-    return user_input
+from random import randint, random
 
-noun1 = get_input("noun")
-verb1 = get_input("verb1")
-noun2 = get_input("noun2")
-verb2 = get_input("verb2")
-adjective1 = get_input("adjective1")
+lower_num , higher_num = 1,10
+random_number: int = randint(lower_num, higher_num)
 
-story = f"""
-Once upon a time, there was a {adjective1} {noun1} who loved to {verb1} all day.
+print(f"Guess the number in the range from  {lower_num} to {higher_num}")
 
-One day, {noun2} walked into the room and caught the {noun1} in the act. 
+while True:
+    try:
+        user_guess : int = int(input("Guess : "))
+    except ValueError as e:
+        print(f"please enter a valid number not this {e}")
+        continue
+    if user_guess > random_number:
+        print("the number is lower")
+    elif user_guess <  random_number:
+        print("the number is higher")
+    else:
+        print("You guessed it")
+        break
 
-{noun2}: "What are you doing?"
-{noun1}: "I'm just {verb1}ing, what's the big deal?"
-{noun2}: "Well, it's not every day that you see a {noun1} {verb1}ing in the middle of the day."
-{noun1}: "I guess you're right. Maybe I should take a break."
-{noun2}: "That's probably a good idea. Why don't we go {verb2} instead?"
-{noun1}: "Sure, that sounds like fun!"
-
-And so, {noun2} and the {noun1} went off to {verb2} and had a great time. 
-The end.
-"""
-print(story)
